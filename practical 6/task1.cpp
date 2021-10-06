982277841d0d312c918ada41042726547b3c76f6
Vijaylatwal12 <91174491+Vijaylatwal12@users.noreply.github.com>
2021-10-06 10:04:17 +0530

    Create task1.cpp

---
+++ practical 6/task1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a=34;
+   float b=45.67;
+   string c="hi_there";
+   cout<<a+a<<endl;
+   cout<<b+b<<endl;
+   cout<<a+b<,endl;
+   cout<<a+c<<endl;
+   cout<<b+c;
+  return 0;
+}
+     
