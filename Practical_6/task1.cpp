e8cfc617ca17d3617bf1d99997fb36ed8d107535
Vijaylatwal12 <91174491+Vijaylatwal12@users.noreply.github.com>
2021-10-06 09:37:28 +0530

    Create task1.cpp

---
+++ Practical_6/task1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=20;
+  int b=45;
+  float c=56.66;
+  float d=78.66;
+  string e="hi";
+  string f="there";
+  cout<<a+b;
+  
+  
+  
+  
+   
+  
