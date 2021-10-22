99537404d23c08b6f18ca5e1a776c996031f71b2
Vijaylatwal12 <91174491+Vijaylatwal12@users.noreply.github.com>
2021-10-22 22:01:05 +0530

    Create task7_V2.cpp

---
+++ practical 8/task7_V2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void swap(int &a,int &b)
+{
+	int t=a;
+	a=b;
+	b=t;
+}
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a and b"<<endl;
+	cin>>a>>b;
+	cout<<"Before Swapping a & b: "<<a<<" "<<b<<endl;
+	swap(a,b);
+	cout<<"After Swapping a & b: "<<a<<" "<<b<<endl;
+	
+}
