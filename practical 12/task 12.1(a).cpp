54392770216d997cf8f4f03c244b752a5a44b883
Vijaylatwal12 <91174491+Vijaylatwal12@users.noreply.github.com>
2022-01-09 15:01:39 +0530

    Create task 12.1(a).cpp

---
+++ practical 12/task 12.1(a).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class A{
+ public:
+     int x;
+ protected:
+     int y;
+ private:
+      int z;
+     };
+  class B:public A{
+  public:
+  void display(){
+  x=20;
+  y=40;
+  // here we cant use z=10; as z is private in parent class
+  cout<<x<<endl<<y<<endl;}
+
+  };
+  int main()
+  {
+
+  B obj;
+  obj.x=15;
+  //here we can not use y and z with object as y is protected and z is private
+  obj.display();
+  }
